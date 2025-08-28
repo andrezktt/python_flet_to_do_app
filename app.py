6f1a588c20b39e3808335ffa69e0773ead80f1f3
andre_zktt <andrezicatti@gmail.com>
2025-08-28 16:12:02 -0300

    :sparkles: feat: create a page layout

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 import flet as ft
 
 def main(page:ft.Page):
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.title = 'To-Do Flet'
+    
     def add_clicked(e):
-        page.add(ft.Checkbox(label=new_task.value))
+        task_view.controls.append(ft.Checkbox(label=new_task.value))
         new_task.value = ''
         page.update()
         
     new_task = ft.TextField(hint_text="What's needs to be done?")
-
-    page.add(
-        new_task,
-        ft.FloatingActionButton(
-            icon=ft.Icons.ADD,
-            on_click=add_clicked
-        )
+    task_view = ft.Column()
+    view = ft.Column(
+        width=600,
+        controls=[
+            ft.Row(
+                controls=[
+                    new_task,
+                    ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_clicked)
+                ],
+            ),
+            task_view
+        ]
     )
+
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(view)
 
 ft.app(target=main)
